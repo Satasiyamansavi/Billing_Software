e5645e1ac434cce64841f37e45b05c03c03bd0f9
Satasiya Mansvi <mansvi@gmail.com>
2026-05-14 15:15:39 +0530

    first commit

--- billing_project/billing_project/urls.py
+++ billing_project/billing_project/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from billing_app import views
 
 urlpatterns = [
     path('', include('billing_app.urls')),
     path('admin/', admin.site.urls), 
-    path('', views.login_view, name='login'),
-
 ]
